ef6a99ad89634cef149087415bba01227436d4d2
Luan Santos <luanhenriquer8@gmail.com>
2023-08-04 12:40:54 +0100

    first commit

---
+++ src/main/kotlin/me/shockyng/rentacarkotlinjakartaee/HelloApplication.kt
@@ -0,0 +1,9 @@
+package me.shockyng.rentacarkotlinjakartaee
+
+import jakarta.ws.rs.ApplicationPath
+import jakarta.ws.rs.core.Application
+
+@ApplicationPath("/api")
+class HelloApplication : Application() {
+
+}
---
+++ src/main/kotlin/me/shockyng/rentacarkotlinjakartaee/HelloResource.kt
@@ -0,0 +1,14 @@
+package me.shockyng.rentacarkotlinjakartaee
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+
+@Path("/hello-world")
+class HelloResource {
+    @GET
+    @Produces("text/plain")
+    fun hello(): String {
+        return "Hello, World!"
+    }
+}
